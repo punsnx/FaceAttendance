caea9d3a4ebbfa3a39f775ff5268310c142ebc70
Khaopunkungz <79169700+srztmx@users.noreply.github.com>
2022-06-18 16:53:41 +0700

    update

---
+++ JavaScripts/main.js
@@ -0,0 +1,18 @@
+console.log("pun");
+
+function mainLoad() {
+    console.log("run");
+    NavLoad();
+}
+function NavLoad() {
+    document.getElementById("navbt1").innerHTML = "Home";
+    document.getElementById("navbt2").innerHTML = "Sign in";
+    document.getElementById("navbt3").innerHTML = "Sign up";
+    document.getElementById("navbt4").innerHTML = "About";
+    document.getElementById("navbt1").setAttribute("href", "/index.html")
+    document.getElementById("navbt2").setAttribute("href", "/login.html")
+    document.getElementById("navbt3").setAttribute("href", "/auth.html")
+    document.getElementById("navbt4").setAttribute("href", "/about.html")
+}
+
+
