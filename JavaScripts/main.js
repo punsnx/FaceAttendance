21a2006225d1740acb88364d9dd3b0d533c533ec
Khaopunkungz <79169700+srztmx@users.noreply.github.com>
2022-06-18 17:26:12 +0700

    update

--- JavaScripts/main.js
+++ JavaScripts/main.js
@@ -12,7 +12,7 @@ function NavLoad() {
     document.getElementById("navbt1").setAttribute("href", "/index.html")
     document.getElementById("navbt2").setAttribute("href", "/login.html")
     document.getElementById("navbt3").setAttribute("href", "/auth.html")
-    document.getElementById("navbt4").setAttribute("href", "/about.html")
+    document.getElementById("navbt4").setAttribute("href", ".")
 }
 
 
