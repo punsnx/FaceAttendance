d58a2f1ea16e75b30942b22b32a6f627ff6ca24a
Khaopunkungz <79169700+srztmx@users.noreply.github.com>
2022-06-23 01:23:58 +0700

    update

--- src/views/JavaScripts/main.js
+++ src/views/JavaScripts/main.js
@@ -13,32 +13,11 @@ function NavLoad() {
     document.getElementById("navbt2").innerHTML = "Sign in";
     document.getElementById("navbt3").innerHTML = "Sign up";
     document.getElementById("navbt4").innerHTML = "About";
-    document.getElementById("navbt1").setAttribute("href", "http://127.0.0.1:8888");
-    document.getElementById("navbt2").setAttribute("href", "http://127.0.0.1:8888/login");
-    document.getElementById("navbt3").setAttribute("href", "http://127.0.0.1:8888/auth");
+    document.getElementById("navbt1").setAttribute("href", "/");
+    document.getElementById("navbt2").setAttribute("href", "/login");
+    document.getElementById("navbt3").setAttribute("href", "/auth");
     document.getElementById("navbt4").setAttribute("href", ".");
 
 }
-function insertLoginDB(email, user, stdid, fname, lname, stdclass, stdroom, stdnumber, password) {
-    MongoClient.connect(url, function(err, db) {
-      if (err) throw err;
-      var dbo = db.db("FaceAttendance");
-      var myobj = { 
-          Email: email, 
-          User: user,
-          StudentID: stdid,
-          Fname: fname,
-          Lname: lname,
-          stdClass: stdclass,
-          stdRoom: stdroom,
-          stdNumber: stdnumber,
-          Password: password
-      };
-      dbo.collection("customers").insertOne(myobj, function(err, res) {
-        if (err) throw err;
-        console.log("1 document inserted");
-        db.close();
-      });
-    });
-  }
+
  
